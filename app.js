c5da0426b97638a35555dc111a1e653488c79e85
Gustavo Lozada <zelote57@gmail.com>
2024-05-10 22:12:39 -0500

    adding a new class, export a class, using error handler, and post method

--- app.js
+++ app.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const placesRoutes = require('./routes/places-routes');
 
 const app = express();
 
+app.use(bodyParser.json());
+
 app.use('/api/places', placesRoutes);
+
+//manejo de errores
+app.use((error, req, res, next)=>{
+    if(res.headerSent){
+        return next(error);
+    }
+    res.status(error.code || 500);
+    res.json({message : error.message || 'Error desconocido'});
+});
 
 app.listen(3000);
--- routes/places-routes.js
+++ routes/places-routes.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const HttpError = require('../models/http-error');
 
 const router = express.Router();
 
@@ -11,7 +12,6 @@ const DUMMY_PLACES = [
     {
         id: 'p2',
         title: 'Centro Cívico',
-
         creator: 'u1'
     }
 ];
@@ -20,12 +20,41 @@ router.get('/', (req, res, next)=>{
     res.json({places : DUMMY_PLACES});
 });
 
-router.get('/:pid', (req, res, next) => {
-    console.log(req.params.pid);
-    const places = DUMMY_PLACES.find(p => {
+router.get('/:pid', (req, res, next) => {    
+    const place = DUMMY_PLACES.find(p => {
         return p.id === req.params.pid;
     });
+    if (!place){        
+        const error = new Error('Lugar no existe para el id especificado');
+        error.code = 404;
+        next(error);
+    }
+    else{
+        res.json({place});
+    }    
+});
+
+router.get('/users/:uid', (req, res, next)=>{
+    const places = DUMMY_PLACES.find(p => {
+        return p.creator === req.params.uid
+    });    
+
+    if (!places){
+        const error = new HttpError('Lugar no existe para el id de usuario especificado', 404);
+        throw error;
+    }
+
     res.json({places});
+});
+
+router.post('/', (req, res, next)=>{
+    const {title, creator} = req.body;
+    const createdPlace = {
+        title,
+        creator
+    };
+    DUMMY_PLACES.push(createdPlace);
+    res.status(201).json({place:createdPlace});
 });
 
 module.exports = router;
